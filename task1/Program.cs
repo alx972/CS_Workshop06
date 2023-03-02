bb14688c6b0dd1a34c59cbba5b1693ead2e7120e
Alex Slyussarev <alex.slyussarev@yandex.ru>
2023-03-02 10:40:10 +0300

    adding homework #6

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,3 +1,34 @@
 ﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 // 0, 7, 8, -2, -2 -> 2
 // -1, -7, 567, 89, 223-> 3
+
+int InputInt(string message) //целочисленный ввод с клавиатуры
+{
+    Console.Write($"{message} > ");
+    int value;
+    if (int.TryParse(Console.ReadLine(), out value)) // проверка правильности ввода числа
+    {
+        return value;
+    }
+    Console.WriteLine("Вы ввели не число");
+    Environment.Exit(1); // exit code программы при ошибке
+    return 0; // функция возвращает 0, потому что надо что-то возвращать int
+}
+
+int SumPositiveNumbersInArray(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) sum++;
+    }
+    return sum;
+}
+
+int quantityNumbers = InputInt("Укажите количество вводимых чисел");
+int[] arrayOfNumbers = new int[quantityNumbers];
+for (int i = 0; i < quantityNumbers; i++)
+{
+    arrayOfNumbers[i] = InputInt($"Введите {quantityNumbers}-е число");
+}
+Console.WriteLine($"Число введенных положительных чисел равно {SumPositiveNumbersInArray(arrayOfNumbers)}"); 
--- task2/Program.cs
+++ task2/Program.cs
@@ -1,3 +1,56 @@
 ﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
 // y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+const int graph1 = 0;
+const int graph2 = 1;
+
+double InputNumber(string message) // ввод с клавиатуры
+{
+    Console.Write($"{message} > ");
+    double value;
+    if (double.TryParse(Console.ReadLine(), out value)) // проверка правильности ввода числа
+    {
+        return value;
+    }
+    Console.WriteLine("Вы ввели не число");
+    Environment.Exit(1); // exit code программы при ошибке
+    return 0; // функция возвращает 0, потому что надо что-то возвращать int
+}
+
+bool ValidateGraphs((double k, double b)[] graph) // проверка на пересечение
+{
+    if (graph[graph1].k == graph[graph2].k)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+// метод находит точку пересечения графиков. возвращает кортеж значений
+(double intersectX, double intersectY) GraphsIntersect((double k, double b)[] graph)
+{
+    double intersectX = (graph[graph2].b - graph[graph1].b) / (graph[graph1].k - graph[graph2].k);
+    double intersectY = graph[graph1].k*intersectX + graph[graph1].b;
+    return (intersectX, intersectY);
+}
+
+Console.WriteLine("Введите коэффициенты для двух графиков вида y = kx + b");
+// для коэффициентов используем массив кортежей
+(double k, double b)[] graph = new[] {(InputNumber($"k1"),InputNumber($"b1")), 
+                                (InputNumber($"k2"),InputNumber($"b2"))};
+
+if (ValidateGraphs(graph))
+{
+    Console.WriteLine("Точка пересечения для графиков ");
+    Console.WriteLine($"y = {graph[graph1].k:f2}x + {graph[graph1].b:f2}");
+    Console.WriteLine($"y = {graph[graph2].k:f2}x + {graph[graph2].b:f2}");
+    Console.WriteLine(GraphsIntersect(graph));
+}
+else
+{
+    Console.WriteLine("Графики не пересекаются или совпадают");
+}
